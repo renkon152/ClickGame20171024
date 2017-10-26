21ccbe8dc57646b137f2b146f7236b74042bf5e9
Ren Yoshimura <nkc.ren.yoshimura@gmail.com>
2017-10-26 10:58:45 +0900

    玉追加

---
+++ Assets/SphereController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SphereController : MonoBehaviour
+{
+    public void shoot(Vector3 dir)
+    {
+        GetComponent<Rigidbody>().AddForce(dir);
+    }
+    private void OnCollisionEnter(Collision other)
+    {
+        GetComponent<Rigidbody>().isKinematic = true;
+    }
+    private void Start()
+    {
+        shoot(new Vector3(0, 200, 2000));
+    }
+}
+	// Use this for initialization
+
---
+++ Assets/SphereGenerator.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SphereGenerator : MonoBehaviour
+{
+    public GameObject SpherePrefab;
+
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject Sphere =
+                Instatiate(SpherePrefab) as GameObject;
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.MousePosition);
+            Vector3 worldDir = ray.direction;
+            Sphere.GetComponent<IgaguriController>().Shoot(
+                worldDir.normalized * 2000);           
+        }
+	}
+}
